11d950b9e74ea2cca26f4a8017d9c5cd1637a6f9
Yash <yashrastogi@gmail.com>
2020-06-03 00:45:02 +0530

    Optimize

--- modules/count_severity_module.py
+++ modules/count_severity_module.py
@@ -5,22 +5,20 @@ from tabulate import tabulate
 
 def count_severity(path: str):
     countDict = {}
-    fileInfoDF = pd.DataFrame(columns=["File Name", "Critical", "High", "Medium", "Low", "Total", "IP Count"])
+    columns = ["File Name", "Critical", "High", "Medium", "Low", "Total", "IP Count"]
+    fileInfoDF = pd.DataFrame(columns=columns)
 
     for root, _, files in os.walk(path):
         for file in files:
             if str(file).endswith("vulns.xlsx") and "~" not in str(file):
                 df: pd.DataFrame = pd.read_excel(f"{root}/{file}", index_col="S. No.")
                 sevCounts: pd.DataFrame = df["Severity"].value_counts()
-                countDict = {
-                    "Critical": 0,
-                    "High": 0,
-                    "Medium": 0,
-                    "Low": 0,
+                countDict = {key: 0 for key in columns}
+                countDict.update({
                     "File Name": file,
                     "Total": sevCounts.sum() - sevCounts.loc[sevCounts.index == "Info"].sum(),
                     "IP Count": df["IP Address"].nunique(),
-                }
+                })
                 for key in countDict:
                     if key in sevCounts.index:
                         countDict[key] = sevCounts[key]
